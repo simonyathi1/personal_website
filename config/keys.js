ebd77983b521c1f0200da9db7f663c6a30755809
simo <simonyathi1@gmail.com>
2018-11-26 16:22:18 +0200

    Added database connectivity for storing and retrieving messages

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb://simonyathi:Bondlile.99@ds115244.mlab.com:15244/personal_website'
+}
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+const keys = require('./config/keys');
+
+mongoose.connect(keys.MongoURI).then(() => {
+    console.log('Remote MongoDB connected...')
+});
+
 //setup environment variables
 const port = process.env.PORT || 3000;
+const Schema = mongoose.Schema;
 
 //setup template engine
 app.engine('handlebars', exphbs({
@@ -14,7 +23,26 @@ app.set('view engine', 'handlebars');
 //setup express static public folder for css and js images
 app.use(express.static('public'));
 
-app.get('/',(req, res) => {
+//setup bodyparser to encode url
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+//create collection with mongoose
+const Message = mongoose.model('message', new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    message: {
+        type: String
+    }
+}));
+
+app.get('/', (req, res) => {
     res.render('home');
 });
 
@@ -24,6 +52,30 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.render('contact');
+});
+//setup the post method from the form attributes
+app.post('/getMessage', (req, res) => {
+    const newMessage = {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message
+    }
+    new Message(newMessage).save()
+        .then(() => {
+            res.render('inbox')
+        });
+});
+
+app.get('/displayMessage', (req, res) => {
+    Message.find({}, (err, messages) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('displayMessage', {
+                messages: messages
+            })
+        }
+    });
 });
 
 app.get('/portfolio', (req, res) => {
