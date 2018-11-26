ff83d8aec2ac099691522b08e7854eafd0b51d78
simo <simonyathi1@gmail.com>
2018-11-26 12:02:23 +0200

    pushing to github

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const port = process.env.port || 3000;
+//setup environment variables
+const port = process.env.PORT || 3000;
 
 //setup template engine
 app.engine('handlebars', exphbs({
